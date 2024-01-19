74df4958f945472777aee1e61f396588f32f8fc6
TuahAji <157122422+TuahAji@users.noreply.github.com>
2024-01-19 20:24:36 +0700

    Users.js

--- Users.js
+++ Users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema(
       {
             nama: String,
-            email: { type: String, unique: true },
+            email: { type: String, unique: true }, 
             npm: String,
             password: String
       },
