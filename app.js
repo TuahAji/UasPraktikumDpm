c9fc2953f70e8844b0919ec91359aab3d15c6366
TuahAji <157122422+TuahAji@users.noreply.github.com>
2024-01-19 19:39:52 +0700

    app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 
-const mongoUrl = "mongodb+srv://franxx:dytod@cluster0.conmone.mongodb.net/?retryWrites=true&w=majority";
+const mongoUrl = "mongodb+srv://TuahAjiSaka:<password>@cluster0.6y3gnv7.mongodb.net/?retryWrites=true&w=majority";
 
 const JWT_SECRET = "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jdsds039[]]pou89ywe";
 
